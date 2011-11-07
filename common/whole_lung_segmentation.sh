d9a9a4471f6638833d25dc84900a0c18e3713550
Gang Song <songgang@seas.upenn.edu>
2011-11-07 14:04:18 -0500

    ENH: add copy orientation in lung segmetation script using perl and c3d

--- common/whole_lung_segmentation.sh
+++ common/whole_lung_segmentation.sh
@@ -51,15 +51,33 @@ SmoothLobeDivisionMask=$TEMPDIR/${IMAGENAME}-smooth-lobe-division.nii.gz
 # using only the first step and the last smooth step
 # output a binary lung mask (original label==2)
 echo 1
-# MYDO ${UTILITIESDIR}/ExtractLungs $InputLung $FirstRoundLungMask
+MYDO ${UTILITIESDIR}/ExtractLungs $InputLung $FirstRoundLungMask
 echo 4
-# MYDO ${UTILITIESDIR}/SmoothLungs $FirstRoundLungMask $SmoothLobeDivisionMask 15
+MYDO ${UTILITIESDIR}/SmoothLungs $FirstRoundLungMask $SmoothLobeDivisionMask 15
 echo 5
-# MYDO cp $FirstRoundLungMask $OUTPUTLABEL
-# MYDO $C3D $SmoothLobeDivisionMask -threshold 2 2 1 0 -o $OUTPUTLABEL
+MYDO cp $FirstRoundLungMask $OUTPUTLABEL
+MYDO $C3D $SmoothLobeDivisionMask -threshold 2 2 1 0 -o $OUTPUTLABEL
 
-# reset the orient code to RPI for Kobe lung database
-MYDO $C3D $OUTPUTLABEL -orient RPI -o $OUTPUTLABEL 
+# reset the orient code to the same as original input
+# as Nick's code did not reset this.
+
+# get the orientation code from file using c3d
+# $ echo $a
+# Image #1: dim = [512, 512, 318]; bb = {[-199.64 183.905 1523.25], [169 552.545 1841.25]}; vox = [0.72, 0.72, 1]; range = [0, 1]; orient = RPI
+# $ echo $a | perl -p -e "s/^.*orient = //g" | perl -p -e "s/;.*//g"
+# RPI
+function GetOrientationFromFile() 
+{
+  if [ -f $1 ]; then 
+    local a=`$C3D $1 -info`
+    echo `echo $a | perl -p -e "s/^.*orient = //g" | perl -p -e "s/;.*//g"`
+  fi;
+}
+
+origOrientation=$(GetOrientationFromFile $INPUTIMAGE)
+
+echo "original input orientation is $origOrientation"
+MYDO $C3D $OUTPUTLABEL -orient $origOrientation -o $OUTPUTLABEL 
 
 if [ 1 ]; then
 echo "this command only for displaying"
