bc105779d5e20e861187b65cc55c71633d1faba9
Gang Song <songgang@seas.upenn.edu>
2012-04-30 21:58:17 -0400

    ENH: convert deformation and principal direction to vtk

---
+++ cuneyt_jan2012/batch_lung_convert_deformation_field_to_vtk.sh
@@ -0,0 +1,105 @@
+#!/bin/bash
+## -S /bin/bash
+
+
+# batch to convert the deformation field to vtk
+# iterating over the fixed image file names
+
+
+commonScriptDir=/home/songgang/project/Cuneyt/Jan2012/script/SongPipeline/cuneyt_jan2012
+
+srcDir=/home/songgang/project/Cuneyt/Jan2012/input/gzipped
+qsuboutDir=/home/songgang/project/Cuneyt/Jan2012/output
+
+# currentDir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )";
+# echo $currentDir;
+
+# pathList="
+# DALLAS-PRE/SUPINE-30cm/DALLAS-PRE-SUPINE-30cm
+# DALLAS-POST/PRONE-10cm/DALLAS-POST-PRONE-10cm
+# "
+
+# <<haha
+pathList="
+ALVIN-POST/PRONE-30cm/ALVIN-POST-PRONE-30cm
+ALVIN-POST/SUPINE-30cm/ALVIN-POST-SUPINE-30cm
+ALVIN-PRE/SUPINE-30cm/ALVIN-PRE-SUPINE-30cm
+
+BARTLETT-POST/PRONE-30cm/BARTLETT-POST-PRONE-30cm
+BARTLETT-POST/SUPINE-30cm/BARTLETT-POST-SUPINE-30cm
+BARTLETT-PRE/SUPINE-30cm/BARTLETT-PRE-SUPINE-30cm
+
+CLYDE-POST/PRONE-30cm/CLYDE-POST-PRONE-30cm
+CLYDE-POST/SUPINE-30cm/CLYDE-POST-SUPINE-30cm
+CLYDE-PRE/SUPINE-30cm/CLYDE-PRE-SUPINE-30cm
+
+DALLAS-POST/PRONE-30cm/DALLAS-POST-PRONE-30cm
+DALLAS-POST/SUPINE-30cm/DALLAS-POST-SUPINE-30cm
+DALLAS-PRE/SUPINE-30cm/DALLAS-PRE-SUPINE-30cm
+
+EVANT-POST/PRONE-30cm/EVANT-POST-PRONE-30cm
+EVANT-POST/SUPINE-30cm/EVANT-POST-SUPINE-30cm
+EVANT-PRE/SUPINE-30cm/EVANT-PRE-SUPINE-30cm
+
+FLINT-POST/PRONE-30cm/FLINT-POST-PRONE-30cm
+FLINT-POST/SUPINE-30cm/FLINT-POST-SUPINE-30cm
+FLINT-PRE/SUPINE-30cm/FLINT-PRE-SUPINE-30cm
+
+GILMER-POST/PRONE-30cm/GILMER-POST-PRONE-30cm
+GILMER-POST/SUPINE-30cm/GILMER-POST-SUPINE-30cm
+GILMER-PRE/SUPINE-30cm/GILMER-PRE-SUPINE-30cm
+
+"
+#haha
+
+
+
+
+pathList=( ${pathList} ) # convert to array from string
+nbPath=${#pathList[@]} # get number of elements in array
+
+echo "Total $nbPath files"
+
+
+for (( i=0; i<nbPath; i++ )); do
+	pathA=${pathList[i]};
+
+	dogtiming=`perl -e '$_="'$pathA'"; /(.+)\/(.+)\/(.+)/; print $1;'`;
+	pospressure=`perl -e '$_="'$pathA'"; /(.+)\/(.+)\/(.+)/; print $2;'`;
+	imgname=`perl -e '$_="'$pathA'"; /(.+)\/(.+)\/(.+)/; print $3;'`;
+	
+	dog=`perl -e'$_="'$dogtiming'"; /(.+)-(.+)/;print $1;'`;
+	timing=`perl -e'$_="'$dogtiming'"; /(.+)-(.+)/;print $2;'`;
+	
+	pos=`perl -e'$_="'$pospressure'"; /(.+)-(.+)/;print $1;'`;
+	pressure=`perl -e'$_="'$pospressure'"; /(.+)-(.+)/;print $2;'`;
+	
+	# verify names
+	imgnameA="$dog-$timing-$pos-$pressure";
+	if [ $imgname != $imgnameA ];
+	then
+		echo "!!!!!!!!!!!!!! inconsistent file names: ";
+		echo "   path: $pathA";
+		echo "   imagname as substring from path: $imgname";
+		echo "   imgname constructed from path: $imgnameA";
+	fi;
+	
+	if [ ! -f $srcDir/${pathA}.hdr ];
+	then
+		echo "!!!!!!!!!!!!!!!!!!!!! file not exist: $srcDir/${pathA}.hdr";
+		break;
+	fi;
+	
+
+  echo -e "pathA: \t$pathA"
+	echo -e "dog: \t$dog"
+	echo -e "timing: \t$timing"
+	echo -e "pos: \t$pos"
+	echo -e "pressuure: \t$pressure"
+
+  qsub -pe serial 2 -e $qsuboutDir -o $qsuboutDir -j y $commonScriptDir/convert_deformation_field_to_vtk.sh $srcDir/${pathA}.hdr
+  # bash $commonScriptDir/convert_deformation_field_to_vtk.sh $srcDir/${pathA}.hdr
+
+	# break;
+
+done;
---
+++ cuneyt_jan2012/convert_deformation_field_to_vtk.sh
@@ -0,0 +1,94 @@
+#!/bin/bash
+#$ -S /bin/bash
+
+
+# convert the deformation field in the vtk format
+# input
+#   fixImage
+#   fixMask
+# output is the default paraview directory
+
+ORIGFIXEDIMAGE=$1
+
+# output is the default paraview directory
+
+function ConvertDeformationToVTK
+{
+  # for Cuneyt's pipeline
+  # convert the defomration field to vtk of vectors
+  ORIGFIXEDIMAGE=$1
+
+  fixImageName=$(basename $ORIGFIXEDIMAGE)
+  fixImageName=${fixImageName%%.*}
+  echo $fixImageName
+  dog=$( perl -e '@a=split("-", "'$fixImageName'");print @a[0]');
+  timing=$( perl -e '@a=split("-", "'$fixImageName'");print @a[1]');
+  pos=$( perl -e '@a=split("-", "'$fixImageName'");print @a[2]');
+  pressure=$( perl -e '@a=split("-", "'$fixImageName'");print @a[3]');
+
+#  PVDIR=/home/songgang/project/Cuneyt/Jan2012/output/${dog}-${timing}/${pos}-${pressure}/LobeMasks/ParaviewFiles
+  PVDIR=/home/songgang/project/Cuneyt/Jan2012/output/${dog}-${timing}/${pos}-fix-30cm-mov-10cm/LobeMasks/ParaviewFiles 
+  mkdir -p $PVDIR
+
+  C3D=/home/songgang/pkg/bin/c3d
+
+  AVANTS=/home/songgang/project/tustison/Utilities/gccrel/ConvertDeformationFieldToAvantsLandmarkFiles
+  # this version is for xvec/yvec/zvec format files
+  # AVANTS=/home/tustison/Utilities/bin64/ConvertDeformationFieldToAvantsLandmarkFiles
+  AVANTS2=/home/tustison/Utilities/bin64/ConvertAvantsLandmarkFileToVTK
+
+  TOTALVECTORFIELD=/home/songgang/project/Cuneyt/Jan2012/output/${dog}-${timing}/${pos}-fix-30cm-mov-10cm/${dog}-${timing}-${pos}-fix-30cm-mov-10cmTotalWarp.nii.gz
+
+  FIXEDMASK=/home/songgang/project/Cuneyt/Jan2012/output/${dog}-${timing}/${pos}-${pressure}/${dog}-${timing}-${pos}-${pressure}-lungmask.nii.gz
+
+  
+
+
+  # input will be the deformation field and the masks
+  if [ ! -f $TOTALVECTORFIELD ];
+  then
+    echo -e "$TOTALVECTORFIELD : not existed!!"
+        exit;
+  fi;
+
+  if [ ! -f $FIXEDMASK ];
+  then
+    echo -e "$FIXEDMASK: not existed!!";
+        exit;
+  fi;
+
+
+
+  # temporarily disabled
+# $AVANTS 3 $TOTALVECTORFIELD ${PVDIR}/${dog}-${timing}-${pos}-TotalWarp 1 $FIXEDMASK
+# $AVANTS2 ${PVDIR}/${dog}-${timing}-${pos}-TotalWarpFixed.txt ${PVDIR}/${dog}-${timing}-${pos}-TotalWarpMoving.txt ${PVDIR}/${dog}-${timing}-${pos}-TotalWarp 2
+#/bin/gzip ${PVDIR}/${dog}-${timing}-${pos}-TotalWarpFixed.txt
+#/bin/gzip ${PVDIR}/${dog}-${timing}-${pos}-TotalWarpMoving.txt
+#/bin/gzip ${PVDIR}/${dog}-${timing}-${pos}-TotalWarpFixed.vtk
+#/bin/gzip ${PVDIR}/${dog}-${timing}-${pos}-TotalWarpMoving.vtk
+
+# recomputing principle vector field in the same directory of total vector field
+NICKPS=/home/songgang/project/tustison/Utilities/gccrel/CreatePrincipalStrainImages
+PSVECOUTPUTPRE=/home/songgang/project/Cuneyt/Jan2012/output/${dog}-${timing}/${pos}-fix-30cm-mov-10cm/${dog}-${timing}-${pos}-fix-30cm-mov-10cm_PSvec
+PS1VECTORFIELD=${PSVECOUTPUTPRE}1.nii.gz
+
+
+$NICKPS 3 $TOTALVECTORFIELD $PSVECOUTPUTPRE 0 $FIXEDMASK
+# not needed any more, have changed Nick's code
+# change the image header since PS1 from Nick's output is different from the original input
+# $C3D $FIXEDMASK $PS1VECTORFIELD -copy-transform -o $PS1VECTORFIELD
+
+$AVANTS 3 $PS1VECTORFIELD ${PVDIR}/${dog}-${timing}-${pos}-PS1 1 $FIXEDMASK
+$AVANTS2 ${PVDIR}/${dog}-${timing}-${pos}-PS1Fixed.txt ${PVDIR}/${dog}-${timing}-${pos}-PS1Moving.txt ${PVDIR}/${dog}-${timing}-${pos}-PS1 2
+# zip the files
+/bin/gzip -f ${PVDIR}/${dog}-${timing}-${pos}-PS1Fixed.txt
+/bin/gzip -f ${PVDIR}/${dog}-${timing}-${pos}-PS1Moving.txt
+/bin/gzip -f ${PVDIR}/${dog}-${timing}-${pos}-PS1Fixed.vtk
+/bin/gzip -f ${PVDIR}/${dog}-${timing}-${pos}-PS1Moving.vtk
+
+
+}
+
+ConvertDeformationToVTK $ORIGFIXEDIMAGE 
+
+
