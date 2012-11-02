29b74db0ba8407ef0840b4da1a0003ca69f5d1cf
Gang Song <songgang@seas.upenn.edu>
2012-11-02 18:58:28 -0400

    remove temporary files and some image files out of feature_selection directory

--- eduardo_ILD/feature_selection/mrmr_img/qsub_tmp/thres-15.sh
+++
@@ -1,3 +0,0 @@
-#!\bin\bash
-
-/home/local/matlab_r2007a/bin/matlab -nojvm -nosplash -nodisplay -r "cd /home/songgang/project/ATCases/script; startup; mrmr_threshold_value=0.4865; job_id='thres-15'; lambda=1e-3; data_path='/home/songgang/project/ATCases/script/mrmr_tmp', script_mrmr_svm_leave_one_out_qsub; exit;"
